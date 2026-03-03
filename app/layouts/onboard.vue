1b1bbc883f835aa3c0725351d3891f3383bd15a5
Nghiatruonggg <nghiatruong110700@gmail.com>
2026-03-03 15:12:32 +1100

    wip

---
+++ app/layouts/onboard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import BaseHeader from "~/components/shared/BaseHeader.vue";
+</script>
+
+<template>
+  <div class="min-h-screen bg-bgColor">
+    <BaseHeader>
+      <template #logo>
+        <div>
+          <img
+            src="../assets/logo/FundedrLogo.png"
+            alt="FundedrLogo"
+            class="max-w-32 object-cover"
+          />
+        </div>
+      </template>
+      <template #right>
+        <button
+          class="text-neutral-100 bg-primary-900 px-4 py-2 font-semibold rounded-lg"
+        >
+          Logout
+        </button>
+      </template>
+    </BaseHeader>
+    <main class="mx-auto max-w-280.5 px-6 py-12">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ app/pages/onboard/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import CardSection from "~/components/onboard/CardSection.vue";
+
+definePageMeta({ layout: "onboard" });
+</script>
+
+<template>
+  <div class="mb-10">
+    <h1 class="text-neutral-900 text-center text-[18px] font-bold mb-2">
+      What is your role on Fundedr?
+    </h1>
+    <h2 class="text-neutral-600 text-center text-[12px] font-medium">
+      Choose the role that best describes your intent.
+    </h2>
+  </div>
+
+  <CardSection />
+</template>
